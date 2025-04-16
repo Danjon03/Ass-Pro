3ccf889997ddd7c4b9f708260d5909184df522fb
MAJones31 <markajones31@gmail.com>
2025-04-16 12:45:11 -0500

    Update server.js
    Change host from 'localhost' to '0.0.0.0'

--- Daniel J/server.js
+++ Daniel J/server.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 const init = async () => {
     const server = Hapi.server({ 
         port: 3000,
-        host: 'localhost',
+        host: '0.0.0.0',
         "routes": {
             "cors": {
             "origin": ["*"],
